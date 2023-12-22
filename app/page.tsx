d569456d99ecbee5bead3700c480098b359c4578
Rodrigo Juarez <juarezr100@gmail.com>
2023-12-22 08:49:35 -0300

    feat: add music and playground section

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import {Button} from "@/components/ui/button";
 import Link from "next/link";
 
 export default function Home() {
-  const views = ['dashboard', 'authentication', 'forms', 'cards']
+  const views = ['dashboard', 'authentication', 'forms', 'cards', 'music', 'playground']
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <div className="flex flex-col gap-6 z-10 max-w-5xl w-full items-center justify-center font-mono">
